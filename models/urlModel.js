636e3d28940a9c3867f6813ad6fc6c153ef742c0
oneofthedevs <iamdevchhaniyara@gmail.com>
2020-11-20 10:16:39 +0530

    :fire: Changed itemModel

--- models/urlModel.js
+++ models/urlModel.js
@@ -1,16 +1,25 @@
 const mongoose = require("mongoose");
 
 const itemSchema = mongoose.Schema({
-  originalUrl: {
+  // This is the URL which we need to short
+  longUrl: {
     type: String,
     require: true,
   },
+  // This will store the unique id related to each URL
   shortId: {
     type: String,
     require: true,
   },
-  url: {
+  // This is the actual short URL
+  shortUrl: {
     type: String,
+    require: true,
+  },
+  // This stores how many times users have used the short URL
+  clickCount: {
+    type: Number,
+    default: 0,
   },
 });
 
